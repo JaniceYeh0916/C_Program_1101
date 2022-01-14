ae02d2fde4892b2611c917d0a6c0488c82b03afc
JaniceYeh0916 <91181565+JaniceYeh0916@users.noreply.github.com>
2022-01-14 20:35:21 +0800

    Create 01.c

---
+++ week07.course/01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int v1;
+    scanf("%d" , &v1);
+    for (int n = 1 ; n <= v1 ; n++){
+        if (v1 % n == 0){
+            printf("%d " , n);
+        }
+    }
+    printf("\n");
+}
